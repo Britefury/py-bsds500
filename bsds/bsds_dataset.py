b915a8568beb57f4caee4422791b76491e1299dd
Geoffrey French <brittix1023@googlemail.com>
2017-04-14 08:54:41 +0100

    Implemented support for accessing the augmented BSDS-500 dataset.

--- bsds/bsds_dataset.py
+++ bsds/bsds_dataset.py
@@ -1,18 +1,11 @@
 import os
+import numpy as np
+from PIL import Image
 from skimage.util import img_as_float
+from skimage.color import rgb2grey
 from skimage.io import imread
 from scipy.io import loadmat
 
-
-def _sample_names(dir, subset):
-    names = []
-    files = os.listdir(os.path.join(dir, subset))
-    for fn in files:
-        dir, filename = os.path.split(fn)
-        name, ext = os.path.splitext(filename)
-        if ext.lower() == '.jpg':
-            names.append(os.path.join(subset, name))
-    return names
 
 class BSDSDataset (object):
     """
@@ -42,9 +35,20 @@ class BSDSDataset (object):
         self.images_path = os.path.join(self.data_path, 'images')
         self.gt_path = os.path.join(self.data_path, 'groundTruth')
 
-        self.train_sample_names = _sample_names(self.images_path, 'train')
-        self.val_sample_names = _sample_names(self.images_path, 'val')
-        self.test_sample_names = _sample_names(self.images_path, 'test')
+        self.train_sample_names = self._sample_names(self.images_path, 'train')
+        self.val_sample_names = self._sample_names(self.images_path, 'val')
+        self.test_sample_names = self._sample_names(self.images_path, 'test')
+
+    @staticmethod
+    def _sample_names(dir, subset):
+        names = []
+        files = os.listdir(os.path.join(dir, subset))
+        for fn in files:
+            dir, filename = os.path.split(fn)
+            name, ext = os.path.splitext(filename)
+            if ext.lower() == '.jpg':
+                names.append(os.path.join(subset, name))
+        return names
 
     def read_image(self, name):
         """
@@ -56,6 +60,18 @@ class BSDSDataset (object):
         """
         path = os.path.join(self.images_path, name + '.jpg')
         return img_as_float(imread(path))
+
+    def image_shape(self, name, scale, rot, flip):
+        """
+        Get the shape of the image identified by the sample name (you can
+        get the names from the `train_sample_names`, `val_sample_names` and
+        `test_sample_names` attributes)
+        :param name: the sample name
+        :return: a tuple of the form `(height, width, channels)`
+        """
+        path = os.path.join(self.images_path, name + '.jpg')
+        img = Image.open(path)
+        return img.height, img.width, 3
 
     def ground_truth_mat(self, name):
         """
@@ -114,7 +130,7 @@ class BSDSDataset (object):
         """
         gt = BSDSDataset.load_ground_truth_mat(path)
         num_gts = gt.shape[1]
-        return [gt[0,i]['Segmentation'][0,0] for i in range(num_gts)]
+        return [gt[0,i]['Segmentation'][0,0].astype(np.int32) for i in range(num_gts)]
 
     @staticmethod
     def load_boundaries(path):
@@ -128,3 +144,140 @@ class BSDSDataset (object):
         gt = BSDSDataset.load_ground_truth_mat(path)
         num_gts = gt.shape[1]
         return [gt[0,i]['Boundaries'][0,0] for i in range(num_gts)]
+
+
+class BSDSHEDAugDataset (object):
+    """
+    BSDS HED augmented dataset wrapper
+
+    Given the path to the root of the BSDS dataset, this class provides
+    methods for loading images, ground truths and evaluating predictions
+
+    The augmented dataset can be downloaded from:
+
+    http://vcl.ucsd.edu/hed/HED-BSDS.tar
+
+    See their repo for more information:
+
+    http://github.com/s9xie/hed
+
+    Attribtes:
+
+    bsds_dataset - standard BSDS dataset
+    root_path - the root path of the dataset
+    """
+
+    AUG_SCALES = [
+        '', '_scale_0.5', '_scale_1.5'
+    ]
+
+    AUG_ROTS = [
+        '0.0', '22.5', '45.0', '67.5', '90.0', '112.5', '135.0', '157.5', '180.0', '202.5', '225.0', '247.5',
+        '270.0', '292.5', '315.0', '337.5'
+    ]
+
+    AUG_FLIPS = [
+        '1_0', '1_1'
+    ]
+
+    ALL_AUGS = [(s, r, f) for f in AUG_FLIPS for r in AUG_ROTS for s in AUG_SCALES]
+
+    def __init__(self, bsds_dataset, root_path):
+        """
+        Constructor
+
+        :param bsds_dataset: the standard BSDS dataset
+        :param root_path: the path to the root of the augmented dataset
+        """
+        self.bsds_dataset = bsds_dataset
+        self.root_path = root_path
+
+
+        self.sample_name_to_fold = {}
+        for name in bsds_dataset.train_sample_names:
+            self.sample_name_to_fold[name] = 'train'
+        for name in bsds_dataset.val_sample_names:
+            self.sample_name_to_fold[name] = 'train'
+        for name in bsds_dataset.test_sample_names:
+            self.sample_name_to_fold[name] = 'test'
+
+    def _data_path(self, data_type, scale, rot, flip, name, ext):
+        fold = self.sample_name_to_fold[name]
+        if data_type not in {'data', 'gt'}:
+            raise ValueError("data_type should be 'data' or 'gt', not {}".format(data_type))
+        if scale not in self.AUG_SCALES:
+            raise ValueError("scale should be one of {}, not {}".format(self.AUG_SCALES, scale))
+        if rot not in self.AUG_ROTS:
+            raise ValueError("rot should be one of {}, not {}".format(self.AUG_ROTS, rot))
+        if flip not in self.AUG_FLIPS:
+            raise ValueError("flip should be one of {}, not {}".format(self.AUG_FLIPS, flip))
+        return os.path.join(self.root_path, fold, 'aug_{}{}'.format(data_type, scale), '{}_{}'.format(rot, flip),
+                            '{}{}'.format(os.path.split(name)[1], ext))
+
+    def read_image(self, name, scale, rot, flip):
+        """
+        Load the image identified by the sample name and augmentation
+        parameters.
+        The sample name `name` should come from the `train_sample_names`,
+        `val_sample_names` and `test_sample_names` attributes of a
+        `BSDSDataset` instance.
+        The `scale`, `rot` and `flip` augmentation parameters should
+        come from `AUG_SCALES`, `AUG_ROTS` and `AUG_FLIPS` attributes
+        of the `BSDSHEDAugDataset` class
+        :param name: the sample name
+        :param scale: augmentation scale
+        :param rot: augmentation rotation
+        :param flip: augmentation flip
+        :return: a tuple of the form `(height, width, channels)`
+        """
+        path = self._data_path('data', scale, rot, flip, name, '.jpg')
+        return img_as_float(imread(path)).astype(np.float32)
+
+    def image_shape(self, name, scale, rot, flip):
+        """
+        Get the shape of the image identified by the sample name
+        and augmentation parameters.
+        The sample name `name` should come from the `train_sample_names`,
+        `val_sample_names` and `test_sample_names` attributes of a
+        `BSDSDataset` instance.
+        The `scale`, `rot` and `flip` augmentation parameters should
+        come from `AUG_SCALES`, `AUG_ROTS` and `AUG_FLIPS` attributes
+        of the `BSDSHEDAugDataset` class
+        :param name: the sample name
+        :param scale: augmentation scale
+        :param rot: augmentation rotation
+        :param flip: augmentation flip
+        :return: a (H,W,3) array containing the image, scaled to range [0,1]
+        """
+        path = self._data_path('data', scale, rot, flip, name, '.jpg')
+        img = Image.open(path)
+        return img.height, img.width, 3
+
+    def mean_boundaries(self, name, scale, rot, flip):
+        """
+        Load the ground truth boundaries identified by the sample name
+        and augmentation parameters.
+
+        See the `read_image` method for more information on the sample
+        name and augmentation parameters
+
+        :param name: the sample name
+        :param scale: augmentation scale
+        :param rot: augmentation rotation
+        :param flip: augmentation flip
+        :return: a list of (H,W) arrays, each of which contains a
+        boundary ground truth
+        """
+        path = self._data_path('gt', scale, rot, flip, name, '.png')
+        return self.load_mean_boundaries(path)
+
+    @staticmethod
+    def load_mean_boundaries(path):
+        """
+        Load the ground truth boundaries from the Matlab file
+        at the specified path.
+        :param path: path
+        :return: a list of (H,W) arrays, each of which contains a
+        boundary ground truth
+        """
+        return rgb2grey(img_as_float(imread(path))).astype(np.float32)
